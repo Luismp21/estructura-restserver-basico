08062634b888c9ccf9fa1d5b3369966fa468d3cf
Luis Muñoz Pinilla <105446828+Luismp21@users.noreply.github.com>
2023-08-07 14:53:43 -0400

    Default por updated.js

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 class Server {
   constructor() {
     this.app = express();
-    this.puerto = process.env.PORT;
+    this.puerto = process.env.PORT || 3000;
     this.usuariosPath = "/api/usuarios";
 
     //Middlewares
